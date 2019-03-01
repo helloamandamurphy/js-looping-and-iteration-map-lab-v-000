ee8bb7ac72f352d76c2e10d7b14f2b6a36a0258f
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-01 22:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   const driverobj = drivers.map(function(driver) {
-    const first = driver.split(' ')[0]
-    const last = driver.split(' ')[1]
-  })
+    const first = driver.split(' ')[0];
+    const last = driver.split(' ')[1];
+    
+    return {firstName: first, lastName: last}; 
+  });
 }
