16efdf1013b2d4e7ed269620950adf5cbe4eb7bd
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-01 22:53:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  const lcdrivers = drivers.map(function(driver) {
+  return drivers.map(function(driver) {
     return driver.toLowerCase();
   });
-  return lcdrivers;
 }
 
 function nameToAttributes(drivers) {
-  const driverobj = drivers.map(function(driver) {
+  return drivers.map(function(driver) {
     const first = driver.split(' ')[0];
     const last = driver.split(' ')[1];
     
     return {firstName: first, lastName: last}; 
   });
-  return driverobj;
 }
 
 function attributesToPhrase(drivers) {
